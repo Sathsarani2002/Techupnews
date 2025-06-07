b0387b86d197fbd889dcfe4ba3d7384afb5a0a0d
Sathsarani Geethamali <sathsaranigeethamali2002@gmail.com>
2025-06-07 21:36:05 +0530

    dev info ui added

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,18 +25,23 @@ android {
             )
         }
     }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
+
+    buildFeatures {
+        viewBinding = true
+    }
 }
 
 dependencies {
-
     implementation(libs.appcompat)
-    implementation(libs.material)
+    implementation(libs.material) // This is needed for ShapeableImageView
     implementation(libs.activity)
     implementation(libs.constraintlayout)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
